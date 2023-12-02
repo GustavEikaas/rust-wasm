b8b9cc4d1bf808b5c0fd3f6e31bf6139b2cd6caf
Gustav Eikaas <gustav.eikaas@gmail.com>
2023-12-02 20:44:56 +0100

    feat: add wasm implementation of add

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
 mod utils;
-
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn warning(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Rust will take over the js world");
-    alert("You cant stop me!");
+pub struct Math {}
+
+#[wasm_bindgen]
+impl Math {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Math {
+        return Math {};
+    }
+
+    pub fn add(&self, a: i32, b: i32) -> i32 {
+        return a + b;
+    }
 }
--- www/src/main.tsx
+++ www/src/main.tsx
@@ -1,3 +1,24 @@
-import * as wasm from "rust-wasm";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { Math } from "rust-wasm";
 
-wasm.greet();
+const Main = () => {
+  return (
+    <div>
+      <div>Hello rust-wasm</div>
+      <button
+        onClick={() => {
+          const r = new Math();
+          console.log(r.add(10, 15));
+        }}
+      >
+        add
+      </button>
+    </div>
+  );
+};
+
+const el = document.getElementById("root")!;
+
+const a = createRoot(el);
+a.render(<Main />);
