d58a4c8412d001a2e4c89f12af7a3ae8894eff80
Gustav Eikaas <gustav.eikaas@gmail.com>
2023-12-02 20:09:52 +0100

    initial project setup

---
+++ www/vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+export default defineConfig({ plugins: [wasm(), topLevelAwait()] });
